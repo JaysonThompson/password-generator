41fb35f3ff45f8358c0f85159e1b31a39deb990f
Jayson Thompson <jaysonthompson922@gmail.com>
2023-05-27 15:39:02 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+]
+
+let password1El = document.querySelector('#password-1')
+let password2El = document.querySelector('#password-2')
+
+let passwordLength = 15
+
+function getRandomChars() {
+  let randomChars = characters[Math.floor(Math.random() * characters.length)]
+  return randomChars
+}
+
+function generatePassword() {
+  let password = ''
+  for (let i = 0; i < passwordLength; i++) {
+    password += getRandomChars()
+  }
+  return password
+}
+
+function printPassword() {
+  password1El.textContent = generatePassword()
+  password2El.textContent = generatePassword()
+}
